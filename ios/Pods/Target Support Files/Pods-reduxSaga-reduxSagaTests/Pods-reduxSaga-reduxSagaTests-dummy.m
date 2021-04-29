c134e4b907af80f274a7be3780631a9ff7b5a07d
Viktor Ponomarenko <vitpon93@gmail.com>
2021-04-29 19:04:21 +0300

    Removed unnecessary files

--- ios/Pods/Target Support Files/Pods-reduxSaga-reduxSagaTests/Pods-reduxSaga-reduxSagaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_reduxSaga_reduxSagaTests : NSObject
-@end
-@implementation PodsDummy_Pods_reduxSaga_reduxSagaTests
-@end
--- ios/Pods/Target Support Files/Pods-reduxSaga-tvOSTests/Pods-reduxSaga-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_reduxSaga_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_reduxSaga_tvOSTests
-@end
