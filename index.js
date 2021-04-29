57768cfb91c25b925792fbbba839ff55ce10aec5
Viktor Ponomarenko <vitpon93@gmail.com>
2021-04-29 19:11:10 +0300

    Fixed uuid issue in DEV mode;

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+
+if (__DEV__ && typeof global.crypto !== "object") {
+  global.crypto = {
+    getRandomValues: (array: any[]) =>
+      array.map(() => Math.floor(Math.random() * 256)),
+  };
+}
 
 AppRegistry.registerComponent(appName, () => App);
